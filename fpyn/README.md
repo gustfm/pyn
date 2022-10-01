6e0c1edc5df0c530e31c801e5eae2d93b82b38a6
gustfm <gustavo.silva6785@gmail.com>
2022-09-30 21:31:50 -0300

    details

--- fpyn/README.md
+++ fpyn/README.md
@@ -2,8 +2,7 @@
 
 ### Missing
 -> Colocar o valor que informa o mês em questão em cada default container usando props;
--> Criar a lista de itens;
 -> Botão de adicionar novo item;
--> Sincronizar cada botão a cada rota e fazer funcionar esses botões redirecionando quando clica;
--> Fazer o checkbox funcioanr e computar os dados;
--> 
+-> Separar os valores do topo em componente;
+-> Separar a tabela em componentes;
+-> Melhoras os nomes kkkk
--- fpyn/src/app/Expense/Expenses.vue
+++ fpyn/src/app/Expense/Expenses.vue
@@ -1,21 +1,29 @@
 <template>
   <div>
-    <h2 class="expenses__title">Expenses</h2>
-
-    <div class="bills">
-      <div class="bills__informations">
-        <small>Incomes</small>
-        <div class="bills__informations__results">
-          <small>R$</small>
-          <h4>7.321,00</h4>
+    <div class="expenses">
+      <div class="bills">
+        <div class="bills__informations">
+          <small>Incomes</small>
+          <div class="bills__informations__results">
+            <small>R$</small>
+            <h4>7.321,00</h4>
+          </div>
         </div>
-      </div>
 
-      <div class="bills__informations">
-        <small>Expenses</small>
-        <div class="bills__informations__results">
-          <small>R$</small>
-          <h4>321,00</h4>
+        <div class="bills__informations">
+          <small>Expenses</small>
+          <div class="bills__informations__results">
+            <small>R$</small>
+            <h4>321,00</h4>
+          </div>
+        </div>
+
+        <div class="bills__informations">
+          <small>Remaining</small>
+          <div class="bills__informations__results">
+            <small>R$</small>
+            <h4>321,00</h4>
+          </div>
         </div>
       </div>
     </div>
@@ -37,32 +45,59 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
+import { ArrowUpCircleIcon, ChevronUpIcon } from "vue-feather-icons";
+
 import ExpensesHeader from "@/app/Expense/ExpensesHeader.vue";
 
 @Component({
-  components: { ExpensesHeader },
+  components: { ExpensesHeader, ArrowUpCircleIcon, ChevronUpIcon },
 })
 export default class Expenses extends Vue {}
 </script>
 
 <style scoped lang="scss">
-.expenses__title {
-  padding-bottom: 10px;
-}
-.bills {
+.expenses {
   display: flex;
-  padding-bottom: 10px;
-}
-.bills__informations {
-  line-height: 1.2;
-  margin-right: 20px;
+  flex-direction: column;
+  // align-items: center;
 
-  &__results {
+  .expenses__title {
+    padding-bottom: 10px;
+  }
+  .bills {
     display: flex;
-    align-items: start;
+    flex-direction: row;
+    justify-content: center;
+    padding: 20px 0;
+  }
+  .bills__informations {
+    line-height: 1.2;
+    margin: 0 20px;
 
-    h4 {
-      line-height: 1;
+    small {
+      color: #57565e;
+    }
+    &__results {
+      display: flex;
+      align-items: start;
+
+      h4 {
+        line-height: 1;
+      }
+
+      .informations__value {
+        display: flex;
+        align-items: center;
+
+        &__icon {
+          width: 13px;
+          height: 13px;
+
+          &--incoming {
+            color: #11b158;
+          }
+        }
+      }
     }
   }
 }
@@ -75,14 +110,21 @@ export default class Expenses extends Vue {}
 
     &__row {
       border-bottom: 1px solid #f5f5f7;
-      
+
       span {
-        border-right: 1px solid #f5f5f7;
+        // border: 1px solid white;
+        // border-right: 1px solid #f5f5f7;
         padding-left: 5px;
-      
+        cursor: pointer;
+        transition: 0.2s ease-in-out;
+        border-radius: 2px;
+
         &:last-child {
           border: 0;
         }
+      }
+      span:hover {
+        background-color: rgba(66, 133, 244, 0.1);
       }
     }
   }
